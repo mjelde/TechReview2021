1530eeeeb3784c94d99adf6d87ed546ede31a19e
sharon <sharon.alexander@seattlecolleges.edu>
2021-02-25 14:15:44 -0800

    Model Commit

--- tech/models.py
+++ tech/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TechType(models.Model):
+    typename=models.CharField(max_length=255)
+    typedescription=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.typename
+
+    class Meta:
+        db_table='techtype'
+
+class Product(models.Model):
+    productname=models.CharField(max_length=255)
+    producttype=models.ForeignKey(TechType, on_delete=models.DO_NOTHING)
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    dateentered=models.DateField()
+    price=models.DecimalField(max_digits=6, decimal_places=2)
+    producturl=models.URLField()
+    description=models.TextField()
+
+    def __str__(self):
+        return self.productname
+
+    class Meta:
+        db_table='product'
+
+class Review(models.Model):
+    title=models.CharField(max_length=255)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    reviewdate=models.DateField()
+    reviewtext=models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table='review'
