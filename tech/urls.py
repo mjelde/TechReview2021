95c3e5ad14defb0506b9fd863bf7690024134405
sharon <sharon.alexander@seattlecolleges.edu>
2021-02-27 15:31:56 -0800

    second product view commit

--- tech/urls.py
+++ tech/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/', views.products, name='products'),
 ]
--- tech/views.py
+++ tech/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Product, TechType, Review
 
 # Create your views here.
 def index(request):
     return render(request, 'tech/index.html')
+
+def products(request):
+    product_list=Product.objects.all()
+    return render(request, 'tech/products.html', {'product_list': product_list})
