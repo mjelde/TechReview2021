0c19483422349024a8f3ebc39433389409f6c7a8
sharon <sharon.alexander@seattlecolleges.edu>
2021-02-28 16:49:59 -0800

    Detail commit

--- tech/urls.py
+++ tech/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
+    path('productDetail/<int:id>', views.productDetail, name='detail'),
 ]
--- tech/views.py
+++ tech/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, TechType, Review
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,8 @@ def index(request):
 def products(request):
     product_list=Product.objects.all()
     return render(request, 'tech/products.html', {'product_list': product_list})
+
+def productDetail(request, id):
+    product=get_object_or_404(Product, pk=id)
+    return render(request, 'tech/productdetail.html', {'product' : product})
+
